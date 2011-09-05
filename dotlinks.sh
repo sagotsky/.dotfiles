a85f33c7c9bcef99c95e5bcaa0eb4e8e64eaaaf5
Jon Sagotsky <valadil@gmail.com>
2011-09-05 18:57:10 -0400

    removed temp files.  added linker script

---
+++ dotlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Creates links to dotfiles in this git repo
+
+find $HOME/.dotfiles/ -maxdepth 1 -not -name .git -not -name dotlinks.sh -not -name README |\
+    xargs -n1 ln -vs -t $HOME/
