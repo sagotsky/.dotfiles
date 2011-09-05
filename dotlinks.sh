4942aebbd19b3d45e13a02cdacef8e654bd31ca6
Jon Sagotsky <valadil@gmail.com>
2011-09-05 19:18:01 -0400

    dotlinks ignores ignorefile

--- dotlinks.sh
+++ dotlinks.sh
@@ -2,5 +2,5 @@
 
 # Creates links to dotfiles in this git repo
 
-find $HOME/.dotfiles/ -maxdepth 1 -not -name .git -not -name dotlinks.sh -not -name README |\
+find $HOME/.dotfiles/ -maxdepth 1 -not -name .git* -not -name dotlinks.sh -not -name README |\
     xargs -n1 ln -vs -t $HOME/
