30c5511d243e0de045ef4ee6e373294b48fb980f
Jon Sagotsky <valadil@gmail.com>
2011-12-29 16:26:05 -0500

    Began gitget script
    So far all it does is list your watched repositories.  Given that that's
    all I intended to do at first, I'm calling that a success.  There's a
    lot to add though.  Like searching.  Still unsure if it should pass
    clone instructions back to git or leave that for pipes.

---
+++ scripts/gitget/gitget.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+# gitget
+# command line tool for searching and downloading from github
+
+import json
+import urllib
+from optparse import OptionParser
+from getpass import getuser
+
+# url of github api
+api = 'https://api.github.com/'
+
+# get results from github url.  format them.
+def callapi(url):
+  s = urllib.urlopen(url)
+  response = json.loads( s.read() )
+  t = ''
+  for item in response:
+    for term in format(options):
+      if term in item:
+        t += item[term]
+      else:
+        t += term
+
+  s.close()
+  return t
+
+# get format array to show which items to print
+def format(options):
+  if (options.list):
+    return ['git_url', '\n']
+
+  return ['name', '\n', 'description', '\n', 'git_url', '\n\n']
+
+# figure out what url suffix to use
+def getUrl(options):
+  url = api
+
+  if (options.watch ):
+    url += 'users/' + options.user + '/watched'
+
+  return url
+
+# main
+if __name__ == '__main__':
+  # get options from cli
+  parser = OptionParser()
+  parser.add_option('-w', '--watch',
+      action='store_true',
+      dest='watch',
+      help='Show repositories watched by user'
+      )
+  parser.add_option('-u', '--user',
+      dest='user',
+      help='GitHub username.  Defaults to your local account.',
+      default=getuser()
+      )
+  parser.add_option('-l', '--list',
+      dest='list',
+      action='store_true',
+      help='Only list the URL of the git repositories.'
+      )
+  (options, args) = parser.parse_args()
+
+  url = getUrl(options)
+  response = callapi(url)
+  print response
