481e47da7888f3ca8a7d1bdbfab290487c7ebe63
Jon Sagotsky <valadil@gmail.com>
2011-12-30 10:45:52 -0500

    Cleaned up options for method, assoc array instead of if nest.  Added --repos listing.

--- scripts/gitget/gitget.py
+++ scripts/gitget/gitget.py
@@ -35,32 +35,44 @@ def format(options):
 
 # figure out what url suffix to use
 def getUrl(options):
-  url = api
+  url_suffixes = {
+      'watched'   : 'users/' + options.user + '/watched',
+      'repos'     : 'users/' + options.user + '/repos',
+  }
 
-  if (options.watch ):
-    url += 'users/' + options.user + '/watched'
-
-  return url
+  return api + url_suffixes[options.method]
 
 # main
 if __name__ == '__main__':
   # get options from cli
   parser = OptionParser()
-  parser.add_option('-w', '--watch',
-      action='store_true',
-      dest='watch',
+
+  parser.add_option('-w', '--watch', '--watched',
+      action='store_const',
+      const='watched',
+      dest='method',
       help='Show repositories watched by user'
       )
+
+  parser.add_option('-r', '--repos', 
+      action='store_const',
+      const='repos',
+      dest='method',
+      help='Show repositories owned by user'
+      )
+
   parser.add_option('-u', '--user',
       dest='user',
       help='GitHub username.  Defaults to your local account.',
       default=getuser()
       )
+
   parser.add_option('-l', '--list',
       dest='list',
       action='store_true',
       help='Only list the URL of the git repositories.'
       )
+
   (options, args) = parser.parse_args()
 
   url = getUrl(options)
