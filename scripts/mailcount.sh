0eb3815f63496caecb1d5e8666eb0714aaba1c76
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2012-01-04 21:17:48 -0500

    Updated mailcounter a bit

--- scripts/mailcount.sh
+++ scripts/mailcount.sh
@@ -4,11 +4,17 @@
 # https://github.com/teleshoes/thunderbird-unread-count/blob/master/unread_count-0.0.1-tb-linux.xpi
 
 COUNT=0
-find ~/.thunderbird -name unread-counts | xargs cat | while read line ; do
-  #N=$(echo $line | cut -d: -f1)
-  #COUNT=$(( $COUNT + $N ))
-  COUNT=$(( $COUNT + $(echo $line | cut -d: -f1) ))
-done
+find ~/.thunderbird -name 'unread-counts' |\
+  inotifywait -q --fromfile - -e ACCESS -e CLOSE_WRITE |\
+  cut -f1 -d' ' |\
+  cut -f1 -d: 
+
+
+#| xargs cat | while read line ; do
+  ##N=$(echo $line | cut -d: -f1)
+  ##COUNT=$(( $COUNT + $N ))
+  #COUNT=$(( $COUNT + $(echo $line | cut -d: -f1) ))
+#done
 
 if [ $COUNT -gt -1 ] ; then
   echo "[$COUNT]" 
