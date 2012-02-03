98e8b3850f9c6b22042dd30c9e2ed50fefd1c476
Jon Sagotsky <valadil@gmail.com>
2012-02-02 22:26:13 -0500

    Gave cli-board better dmenu style

--- scripts/cli-board.sh
+++ scripts/cli-board.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# clip board copier
+# lists files in .cli-board in a dmenu
+# copies contents or output (if executable) into clipboard
+
+
 DIR="$HOME/.cli-board/"
-DMENU_OPTS=''
+FONT=' -fn -*-lucida-bold-r-*-*-15-*-*-*-*-*-*-* '
+DMENU_OPTS="-t -i -b -m 0 -nb #ffb -nf black -sb #aa7 -sf white -p cli-board $FONT"
 FILE=$(find $DIR -type f -printf '%f\n'  | dmenu $DMENU_OPTS)
 export DISPLAY=:0.0
 
