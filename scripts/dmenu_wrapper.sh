d755e32e1ad9d8c064fb5bb4512a9f5e1c2d7f80
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2012-12-03 20:39:38 -0500

    Cleaned up extra args in dmenu wrapper

--- scripts/dmenu_wrapper.sh
+++ scripts/dmenu_wrapper.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # just a wrapper for my fav dmenu settings
 
-dmenu_run -b -i -m 0 -sb '#cfb000' -sf '#000' -nf '#fff' -nb '#4a525a' -fn '-*-fixed-bold-r-*-*-14-*-*-*-*-*-*-*'
+ARGS=" -b -m 0 -sb \'\#cfb000\' " #-sf '\#000' -nf '\#fff' -nb '4a525a' -fn '-*-fixed-bold-r-*-*-14-*-*-*-*-*-*-*' "
 
+#exe=`dmenu_path | dmenu $ARGS ${1+"$@"}` && exec $exe
+exe=$(dmenu_path | dmenu $ARGS) && $exe
