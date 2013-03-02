85333b23504ccb09c6ff8ba6a8bfe21870742c36
Jon Sagotsky <sagotsky@gmail.com>
2013-03-01 21:46:34 -0500

    Added markdown preview script.  It watches .md for changes, sends to uzbl

---
+++ scripts/markdown-preview.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Preview markdown file each time it's written to
+
+if [ -e "$@" ] ; then
+  FILE="$@"
+else 
+  FILE="${PWD}/$@"
+fi
+
+OUT="/tmp/markdown_temp.html"
+while [ -x /usr/bin/inotifywait ] && [ -x /usr/bin/markdown ] ; do 
+
+  markdown $FILE >$OUT
+  uzbl $OUT &>/dev/null &
+  PID=$!
+  inotifywait "$FILE" -e MODIFY &>/dev/null
+  kill $PID
+done
