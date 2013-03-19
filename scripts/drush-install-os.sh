0b55ebce79494857e059f0f175fab6dc3e03e9ce
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-03-19 11:58:11 -0400

    Fixed spacing

--- scripts/drush-install-os.sh
+++ scripts/drush-install-os.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 HOST=$1
+for HOST in "$@" ; do 
+  drush $HOST si -y openscholar \
+    --account-name=admin \
+    --account-pass=password \
+    --site-name=$HOST \
+    openscholar_flavor_form.os_profile_flavor=development \
+    openscholar_install_type.os_profile_type=vsite
 
-drush $HOST si -y openscholar \
-  --account-name=admin \
-  --account-pass=password \
-  --site-name=$HOST \
-  openscholar_flavor_form.os_profile_flavor=development \
-  openscholar_install_type.os_profile_type=vsite
-
-drush $HOST uli
+  drush $HOST uli
+done
 echo -e "\a"
