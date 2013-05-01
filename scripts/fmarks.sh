2d8ca198d0ce247ef07c7d5e290fd968f27c0772
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-05-01 14:37:05 -0400

    fmarks figures out its own path if my scripts dir isn't in $PATH

--- scripts/fmarks.sh
+++ scripts/fmarks.sh
@@ -2,6 +2,8 @@
 
 # runs fmarks.pl.  opens result in currently selected browser
 
+basepath=$(dirname $0)
+
 function getWinID() {
    xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" | grep -v "CUT_BUFFER0" | cut -d' ' -f5
 }
@@ -10,12 +12,15 @@ function getWinTitle() {
 }
 
 
-browser=$(active_window.sh)
-url=$(./fmarks.pl)
+browser=$($basepath/active_window.sh)
+url=$($basepath/fmarks.pl)
 
-case $browser in
-  uzbl-core)  uzbl $url  ;;
-  Navigator) firefox $url ;;
-  firefox|google-chrome)    $browser $url;;
-esac
+if [ "$url" != '' ] ; then
+  case $browser in
+    uzbl-core)  uzbl $url  ;;
+    Navigator) firefox $url ;;
+    firefox|google-chrome)    $browser $url;;
+    *) echo "dont know how to open '$url' with '$browser' " ;;
+  esac
+fi
 
