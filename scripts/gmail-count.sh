03436d06590de3ad8ef1f3037df638768eb053ea
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-11-23 21:30:23 -0500

    Added some newlines for xmobar's sake

--- scripts/gmail-count.sh
+++ scripts/gmail-count.sh
@@ -6,20 +6,22 @@
 # gmail.  This uses a cookie exported from firefox (see cookies exporter)
 # instead of a file with your password.  
 
+echo
 while [ -f ~/.gmail-cookies.txt ] ; do
   chmod 400 ~/.gmail-cookies.txt
   CURL=$(curl -s -b ~/.gmail-cookies.txt  https://mail.google.com/mail/feed/atom/)
   ERROR=$(echo $CURL | grep '<HTML>')
 
   if [[ $ERROR != '' ]] ; then
-    echo [?];
+    MSG='[?]';
   else 
     COUNT=$(echo $CURL | grep '<entry>' | wc -l)
 
     if [[ $COUNT != '0' ]] ; then
-      echo ' •' #echo "[$COUNT]"
+      MSG=' •' #echo "[$COUNT]"
     fi
   fi
 
+  echo $MSG
   sleep 15m
 done
