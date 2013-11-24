412ad992271e1682ca5bc7150f188171045b3c01
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-11-23 19:13:26 -0500

    Made a gmail checker
    It uses firefox cookies instead of username/pw.  I wouldn't call it secure, but it doesn't make me queasy like passwords in a plaintext file would.

---
+++ scripts/gmail-count.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# gmail-count.sh
+# 
+# Displays mail count or just a dot when you have unread mail in
+# gmail.  This uses a cookie exported from firefox (see cookies exporter)
+# instead of a file with your password.  
+
+while [ -f ~/.gmail-cookies.txt ] ; do
+  CURL=$(curl -s -b ~/.gmail-cookies.txt  https://mail.google.com/mail/feed/atom/)
+  ERROR=$(echo $CURL | grep '<HTML>')
+
+  if [[ $ERROR != '' ]] ; then
+    echo [?];
+  else 
+    COUNT=$(echo $CURL | grep '<entry>' | wc -l)
+
+    if [[ $COUNT != '0' ]] ; then
+      echo ' •' #echo "[$COUNT]"
+    fi
+  fi
+
+  sleep 15m
+done
