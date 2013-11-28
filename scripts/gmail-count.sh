f00afac505124581779a00c25718d83efa3b1802
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-11-28 14:01:10 -0500

    Kill gmail-count.sh clones
    Maybe it's time I learn to trap signals so these scripts can cleanup...

--- scripts/gmail-count.sh
+++ scripts/gmail-count.sh
@@ -6,7 +6,10 @@
 # gmail.  This uses a cookie exported from firefox (see cookies exporter)
 # instead of a file with your password.  
 
-echo
+# there can be only one
+pidof -x $0 | sed -e "s/$$//" | xargs kill
+
+SLEEP='15m' # new mail check.  will be faster after mail present or errors so they clear sooner
 while [ -f ~/.gmail-cookies.txt ] ; do
   chmod 400 ~/.gmail-cookies.txt
   CURL=$(curl -s -b ~/.gmail-cookies.txt  https://mail.google.com/mail/feed/atom/)
@@ -15,14 +18,16 @@ while [ -f ~/.gmail-cookies.txt ] ; do
 
   if [[ $ERROR != '' ]] ; then
     MSG='[?]';
+    SLEEP='1m';
   else 
     COUNT=$(echo $CURL | grep '<entry>' | wc -l)
 
     if [[ $COUNT != '0' ]] ; then
       MSG=' •' #echo "[$COUNT]"
+      SLEEP='1m';
     fi
   fi
 
   echo $MSG
-  sleep 1m
+  sleep $SLEEP
 done
