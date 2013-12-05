bd0638b69903959b7cb2cc444a562de26718d101
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-12-05 16:54:08 -0500

    Moved wlalpaper symlink.  Dropbox wasn't copying it rihgt.

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -5,28 +5,38 @@
 # a symlink - if there is none, run random to set it up
 
 DIR="$HOME/.wallpaper"
-LINK="$DIR/wallpaper.png"
+LINK="$HOME/.wallpaper.png"
 CURRENT=$(readlink $LINK)
 
-if [[ $CURRENT == '' || "$1" != "new" && "$1" != 'old' ]] ; then
-  IMG=$( find "$DIR/" | shuf -n1 )
-else 
-  WALLPAPERS=($(ls -t $DIR/*.{png,jpg,jpeg}))
-  for i in "${!WALLPAPERS[@]}" ; do
-    if [[ ${WALLPAPERS[$i]} == $CURRENT ]] ; then
-      case "$1" in
-        'new')
-          IMG="${WALLPAPERS[(( $i - 1))]}"
-          break
-          ;;
-        'old')
-          IMG="${WALLPAPERS[(( $i + 1))]}"
-          break
-          ;;
-      esac
+case "$1" in
+  new|old)
+    WALLPAPERS=($(ls -t $DIR/*.{png,jpg,jpeg}))
+    for i in "${!WALLPAPERS[@]}" ; do
+      if [[ ${WALLPAPERS[$i]} == $CURRENT ]] ; then
+        case "$1" in
+          'new')
+            IMG="${WALLPAPERS[(( $i - 1))]}"
+            break
+            ;;
+          'old')
+            IMG="${WALLPAPERS[(( $i + 1))]}"
+            break
+            ;;
+        esac
+      fi
+    done
+    ;;
+
+  current)
+    IMG="$CURRENT"
+    ;;
+
+  *)
+    if [[ $CURRENT == '' || "$1" != "new" && "$1" != 'old' ]] ; then
+      IMG=$( find "$DIR/" | shuf -n1 )
     fi
-  done
-fi
+    ;;
+esac
 
 notify-send M-S-b "${IMG##*/}" &
 feh --bg-fill --no-xinerama "$IMG"
