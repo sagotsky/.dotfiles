62417e9b79f93aa35e047d0da6ae704ac36ea717
Jon Sagotsky <sagotsky@gmail.com>
2013-12-28 21:48:38 -0500

    Began album art script.  Semi-functional.
    Workflow is dumb.  I like last.fm's api and use that to search for albums.  When no album matches, I look at all the albums an artist provides and use the best match (best determined by word count).  But last.fm only gives you 300x300 album art so I take their musicbrainz ids and get the cover from coverartarchive.org.  This has room for improvement.
    
    Search for album/artist determines album/artist by reading id3 tags.  Only works with mp3s and assumes tags exist, which is not always true.  Folder names aren't reliable - sometimes I use artist/album and sometimes artist/album/cd2 comes into play.  Plus I don't want to depend on my collection's structure.
    
    Script isn't at all robust.  Needs some checks to bail out or fix things.
    
    jshon doesn't always get the results I want.  Not sure where the problem is.
    
    The part I'm happy with is the search.  find gets me directories.  Uses execdir to call a function that checks if directory has mp3s.  Execdir is a condition so items that don't have mp3s stop matching at this piont.  Those that do match then get checked if they have a folder.jpg and only when they don't do we search for album art.  Everything else is a hack.

---
+++ scripts/album_art.sh
@@ -0,0 +1,72 @@
+#!/bin/bash 
+
+# find music folders that don't have album art
+
+
+set -a
+function has_mp3() {
+  [[ "$(find "$PWD/$@/" -maxdepth 1 -iname '*.mp3' | wc -l)" != '0' ]] && return 0
+}
+
+function has_albumart() {
+  [[ "$(find "$PWD/$@/" -maxdepth 1 -iname 'folder.jpg' | wc -l)" != '0' ]] && return 0
+}
+
+function get_albumart() {
+  DIR="$@"
+  FILE=$(find "$DIR" -name '*.mp3' | head -n 1)
+  ARTIST=$(mp3info "$FILE" -p %a | tr ' ' +)
+  ALBUM=$(mp3info "$FILE" -p %l | tr ' ' +)
+  
+  mbid=$(curl -s "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=$(cat $HOME/.lastfm)&artist=$ARTIST&album=$ALBUM&format=json" | jshon -e album -e mbid | tr -d '"')
+  if [[ "$mbid" == '' ]] ; then
+    ALBUM=$(fuzzy_album "$ARTIST" "$ALBUM" | tr ' ' +)
+    mbid=$(curl -s "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=$(cat $HOME/.lastfm)&artist=$ARTIST&album=$ALBUM&format=json" | jshon -e album -e mbid | tr -d '"')
+  fi
+  link=$(curl -s "http://coverartarchive.org/release/$mbid" | cut -f 2 -d' ')
+  img=$(curl -s -L "$link" | jshon -e images -e 0 -e image  | tr -d '"')
+
+  [[ "$img" != '' ]] && curl "$img" > "$DIR/folder.jpg" ; echo $ARTIST - $ALBUM
+}
+
+# when there's no clear album, do something clever
+function fuzzy_album() {
+  ARTIST="$1"
+  ALBUM="${2//+/ }"
+  ALL=$(curl -s "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=$ARTIST&api_key=2ae0cdecdc23f5ffad8ef6c5f335258c&format=json&limit=500" | jshon -e topalbums -e album -a -e name | tr -d '"')
+  echo "$ALL" | while read line ; do
+    HIT=$(echo ${line//+/ } $ALBUM |  
+      tr ' ' "\n" |
+      sort |
+      uniq -d -i | 
+      wc -l
+    )
+
+    MISS=$(echo ${line//+/ } $ALBUM |  
+      tr ' ' "\n" |
+      sort |
+      uniq -u -i | 
+      wc -l
+    )
+    
+
+
+    echo $(( ($HIT * 5) - $MISS )) $line
+  done | sort -n | tail -n 1 | cut -f 2- -d' '
+}
+
+set +a
+
+#fuzzy_album rush "moving pictures 2112 live"
+#exit 
+
+find "$@" -type d \
+  -execdir bash -c 'has_mp3 "{}"' \; \
+  -and -not -execdir bash -c 'has_albumart "{}"' \; \
+  -print0 \
+  -print | while read line ; do
+    echo $line
+    #get_albumart "$line"
+  done
+
+
