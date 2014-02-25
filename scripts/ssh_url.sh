b5a015b11d9d8c1b530d69af85ee97b6db4fbd8a
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-02-25 13:55:55 -0500

    Added click handler for work's ssh://user@host links

---
+++ scripts/ssh_url.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# handle ssh:// urls from firefox
+#gconftool-2 -s /desktop/gnome/url-handlers/ssh/enabled --type Boolean false
+
+if [[ "$@" =~ 'ssh://' ]] ; then
+  xterm -e "ssh ${@%%ssh://}"
+fi
+
+
