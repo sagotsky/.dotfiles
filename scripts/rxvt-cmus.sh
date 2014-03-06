0ba2513d9d3d8714c5f771ce1b76b4d7841d3f2d
Jon Sagotsky <sagotsky@gmail.com>
2014-03-06 09:35:59 -0500

    Added special rxvt/cmus window script.
    It listens to a file for getting new bg images

---
+++ scripts/rxvt-cmus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#xterm -fa droidsansmono-16 -bg '#1c1c1c' -fg white -class cmus -e 'TERM=xterm-256color cmus' &
+urxvt -fn "xft:Droid Sans Mono-16" -bd black -b 2 -bg '#1c1c1c' -fg '#fff' +sb -name cmus -e sh -c 'rxvt-background.sh & cmus' &
+
+sleep 1
+cmus-filter.sh -r -l album
+cmus-remote --play
+cmus-remote --stop
