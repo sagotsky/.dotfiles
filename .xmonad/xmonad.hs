0785facee3e30f153778c98589cc28654365e0d4
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-03-29 20:48:54 -0400

    xmonad cleanup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,16 +14,12 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders
--- import XMonad.Layout.Spacing
 import XMonad.Layout.Named
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.Grid  
---import XMonad.Layout.Magnifier
 import XMonad.Layout.Circle
---import XMonad.Layout.ResizableTile
---import XMonad.Layout.Spiral
 import XMonad.Layout.IM
 import XMonad.Layout.ShowWName
 import XMonad.Layout.PerWorkspace
@@ -32,33 +28,26 @@ import XMonad.Util.EZConfig
 import XMonad.Actions.DynamicWorkspaces
 import XMonad.Prompt
 import XMonad.Actions.CycleWS
--- import XMonad.Hooks.FadeInactive -- changes opacity of inactive windows.  when xcompmgr is mature, try this instead of borders to indicate activity
 
 myTerminal            = "x-terminal-emulator"
 myBorderWidth         = 1
-myModMask             = mod4Mask -- use super as meta
+myModMask             = mod4Mask 
 myWorkspaces          = ["web-2", "music-2", "email-3", "term-4", "chat-5", "ide-6", "7", "8", "9", "0"] 
 myNormalBorderColor   = "#444455"
 myFocusedBorderColor  = "#cfb000"
 myUrgentBorderColor   = "#ff5500"
 
-
-myLayout =   -- ewmhDesktopsLayout
-            onWorkspace "music-2" circle $
+myLayout = onWorkspace "music-2" circle $
             onWorkspace "chat-5" circle $
             avoidStruts
            ( smartBorders
-           ( tall |||  Full ||| layoutHints Grid ||| circle ))
+           ( tall |||  Full ||| circle ))
     where
       tall = Tall nmaster delta ratio
-      --wide = avoidStruts $ named "Wide" (Mirror tall)
       circle = layoutHints Circle
       nmaster = 1
       delta = 3/100
       ratio = 1/2
-      -- imLayout = named "chat" $ (withIM (11/64) imApps $ Grid  )
-      -- imApps = Or (ClassName "Skype.real") (Role "contact_list")
---           $ layoutHook defaultConfig
 
 
 myManageHook =  composeAll
@@ -92,53 +81,44 @@ myManageHook =  composeAll
 
 main = do 
   xmproc <- spawnPipe "xmobar"
-  --sp <- mkSpawner
   xmonad $ withUrgencyHook NoUrgencyHook
        $ ewmh defaultConfig
              {terminal = myTerminal
-             --startupHook = myStartupHook
              ,borderWidth = myBorderWidth
              ,modMask = myModMask
              ,workspaces = myWorkspaces
              ,normalBorderColor = myNormalBorderColor
              ,focusedBorderColor = "#dddddf" -- myFocusedBorderColor
---             ,urgentBorderColor = myUrgentBorderColor
-                     
-             --,handleEventHook    = fullscreenEventHook
              ,layoutHook = myShowWName myLayout
-             --,manageHook = manageSpawn sp 
-                            -- <+> manageDocks <+> myManageHook 
              ,manageHook = manageDocks <+> myManageHook 
                            <+> manageHook defaultConfig
              ,logHook = dynamicLogWithPP $ xmobarPP 
              -- http://www.alanwood.net/demos/wgl4.html special chars
-                         { ppOutput = hPutStrLn xmproc
-                         , ppTitle = xmobarColor  "white" "" . shorten 140 . wrap " " " "
-                         , ppUrgent = xmobarColor "white" "" . sed (const "•") ".*[0-46-9]" . sed (const "• ") ".*5"
-                         , ppCurrent = xmobarColor "#ec5500" "" . sed (const "•") ".*[0-46-9]" . sed (const "• ") ".*5"
-                         , ppVisible = xmobarColor "#ec5500" "" .  sed (const "•") ".*[0-46-9]". sed (const "• ") ".*5"
-                         , ppHidden =     xmobarColor "#888888" "" . sed (const "•") ".*[0-9]". sed (const "• ") ".*5"
-                         , ppLayout  = sed (const "") ".*" -- xmobarColor "#aaaaaa" "" . wrap "" ""  
-                         , ppHiddenNoWindows =     xmobarColor "#666666" "" . sed (const "◦") ".*[0-46-9]". sed (const "◦ ") ".*5" -- replace 5 first, then general.
-                         , ppSep =  " " 
-
-                         --, ppHidden  = xmobarColor "#aaaaaa" "" . wrap "" "" 
-                         --Current      workspace with focus
-                         --Visible      displayed workspace without focus
-                         --Hidden       workspace that isn't displayed
-                         --HiddenNoWindows
-                         --Urgent
-                         --  
-                         --Sep string between sections
-                         --WsSep    sep between ws types
-                         --Title    active window title
-                         --Layout   name of current layout
-                         --Order
-                         --Sort
-                         --Extras   time and date, tertiary loggers
-                         --Output   entire string
-
-                         }  
+               { ppOutput = hPutStrLn xmproc
+                 , ppTitle = xmobarColor  "white" "" . shorten 140 . wrap " " " "
+                 , ppUrgent = xmobarColor "white" "" . sed (const "•") ".*[0-46-9]" . sed (const "• ") ".*5"
+                 , ppCurrent = xmobarColor "#ec5500" "" . sed (const "•") ".*[0-46-9]" . sed (const "• ") ".*5"
+                 , ppVisible = xmobarColor "#ec5500" "" .  sed (const "•") ".*[0-46-9]". sed (const "• ") ".*5"
+                 , ppHidden =     xmobarColor "#888888" "" . sed (const "•") ".*[0-9]". sed (const "• ") ".*5"
+                 , ppLayout  = sed (const "") ".*" -- xmobarColor "#aaaaaa" "" . wrap "" ""  
+                 , ppHiddenNoWindows =     xmobarColor "#666666" "" . sed (const "◦") ".*[0-46-9]". sed (const "◦ ") ".*5" -- replace 5 first, then general.
+                 , ppSep =  " " 
+                 --, ppHidden  = xmobarColor "#aaaaaa" "" . wrap "" "" 
+                 --Current      workspace with focus
+                 --Visible      displayed workspace without focus
+                 --Hidden       workspace that isn't displayed
+                 --HiddenNoWindows
+                 --Urgent
+                 --  
+                 --Sep string between sections
+                 --WsSep    sep between ws types
+                 --Title    active window title
+                 --Layout   name of current layout
+                 --Order
+                 --Sort
+                 --Extras   time and date, tertiary loggers
+                 --Output   entire string
+               }  
 
 
 
@@ -148,9 +128,9 @@ main = do
 myKeys = [ 
     -- application shortcuts
 	  ("<XF86HomePage>",   spawn "nautilus") --home browser
-	, ("<XF86Favorites>",  spawn "~/scripts/gdm3switch.sh jenn") --user switch
+    , ("<XF86Favorites>",  spawn "~/scripts/gdm3switch.sh jenn") --user switch
     , ("<XF86Mail>",       spawn "active_win_man.sh") -- manpage for active win
-	, ("<XF86Calculator>", spawn "gnome-calculator") --calc
+    , ("<XF86Calculator>", spawn "gnome-calculator") --calc
     , ("M-g",              spawn "fmarks.sh") -- open FF bookmarks in current browser
     , ("M-C-<Return>",     spawn "urxvt") -- urxvt alternate term is transparent
     , ("M-s", spawn "HOST=$(grep '^host ' .ssh/config | cut -f 2 -d' ' | ~/scripts/dmenu_hist.sh ssh -l 10 -i -s 0 -sb '#cfb000' -sf '#000' -nf '#fff' -nb '#4a525a' -fn -*-terminus-bold-r-*-*-16) && xterm -e 'ssh $HOST' ")  
@@ -159,16 +139,16 @@ myKeys = [
 
     -- music
     , ("<XF86AudioPlay>",        spawn "music-client.sh toggle") --vol up
-	, ("<XF86Forward>",          spawn "music-client.sh next") --Next
-	, ("<XF86Back>",             spawn "music-client.sh back") --Back
-	, ("<XF86AudioRaiseVolume>", spawn "vol-up.sh") --vol up
-	, ("<XF86AudioLowerVolume>", spawn "vol-down.sh") --vol down
-	, ("<XF86AudioMute>",        spawn "vol-mute.sh") --vol mute
-	, ("<XF86LaunchA>",          spawn "music-client.sh rate1") -- rate 1-5
-	, ("<XF86LaunchB>",          spawn "music-client.sh rate2") -- rate 1-5
-	, ("<XF86LaunchC>",          spawn "music-client.sh rate3") -- rate 1-5
-	, ("<XF86LaunchD>",          spawn "music-client.sh rate4") -- rate 1-5
-	, ("<XF86LaunchE>",          spawn "music-client.sh rate5") -- rate 1-5
+    , ("<XF86Forward>",          spawn "music-client.sh next") --Next
+    , ("<XF86Back>",             spawn "music-client.sh back") --Back
+    , ("<XF86AudioRaiseVolume>", spawn "vol-up.sh") --vol up
+    , ("<XF86AudioLowerVolume>", spawn "vol-down.sh") --vol down
+    , ("<XF86AudioMute>",        spawn "vol-mute.sh") --vol mute
+    , ("<XF86LaunchA>",          spawn "music-client.sh rate1") -- rate 1-5
+    , ("<XF86LaunchB>",          spawn "music-client.sh rate2") -- rate 1-5
+    , ("<XF86LaunchC>",          spawn "music-client.sh rate3") -- rate 1-5
+    , ("<XF86LaunchD>",          spawn "music-client.sh rate4") -- rate 1-5
+    , ("<XF86LaunchE>",          spawn "music-client.sh rate5") -- rate 1-5
     , ("M-m", spawn "cmus-filter.sh") -- cmus play song
     , ("M-S-m", spawn "cmus-filter.sh --list album") -- cmus play album
     , ("M-C-m", spawn "cmus-filter.sh --randomize --list album") -- cmus random album
@@ -181,7 +161,7 @@ myKeys = [
     , ("M-S-o", spawn "transset-df -a --inc 1") -- reset transparent
 
     --xcalib screen options
-	, ("<XF86Search>",   spawn "xcalib -a -i") -- screen color invert
+    , ("<XF86Search>",   spawn "xcalib -a -i") -- screen color invert
     , ("C-<XF86Search>", spawn "xcalib -c") -- screen color reset
     , ("S-<XF86Search>", spawn "xcalib -a -co 95") -- screen contrast decrease
     , ("M-<XF86Search>", spawn "xcalib -a -b   5") -- screen brightness increase
@@ -224,31 +204,20 @@ myKeys = [
 
 
 myXPConfig = defaultXPConfig 
-                { bgColor = "#4a525a"
+                { bgColor = "#101015"
                 , fgColor = "#ffffff"
-                , fgHLight = "#000000"
-                , bgHLight = myFocusedBorderColor 
-                , borderColor = myFocusedBorderColor -- "#000000"
+                , fgHLight = "#101015"
+                , bgHLight = "white"
+                , borderColor = "#000000"
                 , promptBorderWidth = 1
                 , height = 35
                 , font = "xft:Verdana:pixelsize=20:antialias=true"
                 
                 }
 
--- you get no guarantee that this works correctly!
---currentNumWindows :: X Int
---currentNumWindows = do
-    --ws <- gets windowset
-    --return $ case ( W.stack $ W.workspace $ W.current ws ) of
-        --(Just s) -> (length $ W.up s) + (length $ W.down s) + (M.size $ W.floating ws) + 1
-        --otherwise -> 0
-
--- intToStr ::  Int -> String
--- intToStr i = show i
-
 myShowWName = showWName' defaultSWNConfig
-	{ swn_color = myFocusedBorderColor
-	, swn_bgcolor = myNormalBorderColor
+	{ swn_color = "#101015"
+	, swn_bgcolor = "white"
 	, swn_font = "xft:Verdana:pixelsize=50:antialias=true"
 	, swn_fade = 0.5 
 	}
