ce11fcac0039a674b1b677166f585848b96907f4
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-04-02 16:44:30 -0400

    Added rails to ri-select.  Made it use rbenv instead of system

--- scripts/ri-menu.sh
+++ scripts/ri-menu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# List all objects and their methods for RI lookup
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+RI="$(which ri)"
 
+# List all objects and their methods for RI lookup
 TERM=$(( /usr/bin/env ruby <<-EOF
+  require 'rails'
   [Class, Module].each do |type| 
     ObjectSpace.each_object(type) do |obj| 
       obj.instance_methods.each do |method|
@@ -14,6 +18,6 @@ EOF
 ) |  dmenu -b -l 20 -i
 )
 
-[[ $? && "$TERM" != "" ]] && xterm -bg '#202025' -fa 'Source Code Pro-10' -e "bash -i -c 'ri $TERM'"
+[[ $? && "$TERM" != "" ]] && xterm -bg '#202025' -fa 'Source Code Pro-10' -e "bash -i -c '$RI $TERM '"
 
 
