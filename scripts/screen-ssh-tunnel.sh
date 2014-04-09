cc25891d6b46b194df46a9bac33644f82ba086b1
Jon Sagotsky <sagotsky@gmail.com>
2014-04-09 12:33:05 -0400

    Let's try adding some keep alive to ssh tunnels.

--- scripts/screen-ssh-tunnel.sh
+++ scripts/screen-ssh-tunnel.sh
@@ -3,10 +3,10 @@
 # set up ssh tunnels to home exactly once but refresh them if they die
 
 ssh-keyed || xterm -e 'ssh-add'
-
+TUNNELS='-R 12345:127.0.0.1:2222 -R 3000:127.0.0.1:3000'
+OPTIONS='-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o BatchMode=yes'
 
 while : ; do
-  autossh -R 12345:127.0.0.1:2222 -R 3000:127.0.0.1:3000 rj
+  autossh $TUNNELS $OPTIONS rj
   sleep 3m
 done
-#nohup autossh -f -R 3000:127.0.0.1:3000 rj
