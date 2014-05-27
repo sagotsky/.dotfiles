670b400055b195a0babd118012fec0e44b7c8983
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-05-27 17:02:34 -0400

    A couple screen change scripts.

--- scripts/boxy-projector.sh
+++ scripts/boxy-projector.sh
@@ -3,11 +3,9 @@
 # Set boxy's screens on X11 startup
 # Add this to lightdm as a display-setup-script
 
+disper -l -d DP1 &>/dev/null && \
 disper -l -d eDP1 &>/dev/null && \
-disper -l -d DP2 &>/dev/null && \
-disper -e DP2,eDP1 -t left
+  disper -e DP1,eDP1 -t left
 
-xrandr --output DP2 --auto --rotate left
-#xmodmap.sh
 xrandr --dpi 96
 wallpaper.sh current
