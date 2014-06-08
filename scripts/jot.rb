d8bbd2e380cf779d00594d21b6896be607651f5f
Jon Sagotsky <sagotsky@gmail.com>
2014-06-08 16:01:03 -0400

    Refactored for ease of adding cli actions
    Not totally satisfied with all the glue yet.  Jot owns actions and
    actions take jot as an arg.  That seems a bit off.  I wanted them to
    just have the options from jot, but then wanted to hit the methods too.
    
    Classes that subclass off of action can be called from jot cli.  The run
    method defines what they do.  Match lets them control what commands they
    match (ie shift matches on up or down).  help defines help message but
    isn't used yet.

--- scripts/jot.rb
+++ scripts/jot.rb
@@ -24,22 +24,58 @@ require 'pry'
 
 module Jot
   class Jot
-    MODES = [:show, :append, :list, :shift, :remove]
     DIR = ENV['HOME'] + '/.jot'
 
-    attr_accessor :opts
+    attr_accessor :action
+    attr_accessor :lines
+    attr_accessor :file
+
 
     def initialize
-      self.opts = Options.new ARGV
+      action = List if ARGV.length == 0
+
+      while ARGV.length > 0 
+        action = Show
+        arg = ARGV.shift
+        
+        case arg
+        when /^@/    #bucket
+          file = arg.gsub /^@/, ''
+          self.file = "#{Jot::DIR}/#{file}"
+          
+        when /^#/     #range
+          p 'tagging'
+          self.tag = arg
+
+        when /^\d+/ #d[\d\-,]*/
+          self.lines = parse_range arg
+          #self.mode = (ARGV.length > 0) ? ARGV.shift.to_sym : self.mode
+          #mode is show or it'll be set
+          
+        #cut off - necessary? might be implicit below
+        when /^--$/
+          #ARGV.shift
+          action ||= Jot::Append
+          break
+
+        else 
+          action = Action::ALL.find{ |klass| klass.match(arg) } 
+          action ||= Append
+          ARGV.unshift(arg)
+          break
+        end
+      end 
+
+      self.action = action.new ARGV
     end
 
     def run
-      p opts
-      self.send(opts.mode) if MODES.include?(opts.mode)
+      puts self.action.run self
     end
 
-    def file_get_lines
-      lines = IO.readlines(opts.file)
+    def file_format_lines
+      # booo!  this is only good for printing.  why is width hardcoded?
+      lines = IO.readlines(file)
       width = lines.length.to_s.length
 
       lines.each_with_index.map do |line, index|
@@ -48,55 +84,11 @@ module Jot
     end
 
     def update_file
-      lines = IO.readlines(opts.file)
+      lines = IO.readlines(file)
       yield lines if block_given?
-      File.open(opts.file, 'w') do |fh|
+      File.open(file, 'w') do |fh|
         fh.puts lines
       end
-    end
-
-
-    # *dent
-    # flip
-
-    # move/copy
-    def shift # up and down map to this
-      
-    end
-
-
-    def remove
-      update_file do |lines|
-        # go backwards to preserve indices
-        opts.lines.sort.reverse.each do |n|
-          lines.delete_at n 
-        end
-      end
-      self.send(:show, true)
-    end
-
-    def show(all=false)
-      puts opts.file
-      file_get_lines.each_with_index do |line, i|
-        puts line if opts.lines.nil? || opts.lines.include?(i) || all
-      end
-    end
-
-    def append
-      update_file do |text|
-        text.push opts.text
-      end
-      self.send(:show, true)
-    end
-
-    def list
-      puts(Dir["#{DIR}/*"].map do |path|
-        "@#{File.basename(path)}"
-      end.join(" "))
-    end
-
-    def edit
-
     end
 
   end
@@ -106,43 +98,7 @@ module Jot
     attr_accessor :file
     attr_accessor :lines
     attr_accessor :text
-    attr_accessor :mode
-
-    def initialize(args = [])
-      self.mode = :list if args.length == 0
-
-      while args.length > 0
-        self.mode = :show
-        arg = args.shift
-        
-        case arg
-
-        when /^@/
-          file = arg.gsub /^@/, ''
-          self.file = "#{Jot::DIR}/#{file}"
-          
-        when /^#/
-          p 'tagging'
-          self.tag = arg
-
-        when /^\d+/ #d[\d\-,]*/
-          self.lines = parse_range arg
-          self.mode = (args.length > 0) ? args.shift.to_sym : self.mode
-          
-          # this is where we start losing things.  range, op is great.  what else?  @jot 1-3 down 2;  taking another arg is fine.  what about indented children?
-          args = []
-
-        when /^--$/
-          self.text = args.shift(args.length).join(' ')
-          self.mode = :append
-
-        else 
-          # is this still sensible?  preshifting arg works well until here.
-          self.text = "#{arg} #{args.shift(args.length).join(' ')}"
-          self.mode = :append
-        end
-      end 
-    end
+    attr_accessor :action
 
     
     def parse_range(range)
@@ -159,6 +115,82 @@ module Jot
         
     def to_s(arg=nil)
       instance_variables.map { |i| "#{i}: #{instance_variable_get i}" }.to_s
+    end
+  end
+
+
+
+  # A command line action.
+  class Action
+    ALL = []
+    attr_accessor :args
+
+    def self.inherited(subclass)
+      ALL << subclass
+    end
+
+    # Returns true if this action should be used.
+    #   Default: match name of class
+    def self.match(arg)
+      self.to_s.downcase == "jot::#{arg.downcase}"
+    end
+
+    def initialize(args)
+      self.args = args
+    end
+
+    # Runs the action defined by this class.  Must be overridden.
+    def run(jot)
+      raise 'Action registered but undefined.'
+    end 
+  end
+
+  class Show < Action
+    def run(jot)
+      jot.file_format_lines.join("\n")
+    end
+
+  end
+
+
+  class List < Action
+    def run(jot)
+      Dir["#{Jot::DIR}/*"].map do |path|
+        "@#{File.basename(path)}"
+      end.join(" ")
+    end
+  end
+
+  class Shift < Action
+    def help
+      'Move line(s) vertically.'
+    end
+
+    def self.match(arg)
+      super || ['up', 'down'].include?(arg)
+    end
+
+    def action(jot)
+
+    end
+
+    # need to know: range, movement arg
+  end
+
+  class Move < Action
+    # move needs 2 whole files and the range
+    def help
+      'Move line(s) to new bucket'
+    end
+  end
+
+  class Append < Action
+
+    def run(jot)
+      jot.update_file do |text|
+        text.push(args.join(' '))
+      end
+      # self.send show?
     end
   end
 end 
@@ -180,8 +212,14 @@ Todo
 - Movement
   - jot @a 1 to @b    ## is this copy or move?  
       bucket, range, cmd, arg
-- What is the point of tags?  Honestly?
-  - Could they also be a range? 
+- what is the point of tags?  honestly?
+  - could they also be a range? 
 
+  VSO jot @bucket up 1 1-4 up 1      # sounds more natural
 
+  SVO jot @bucket 1-4 up 1      # follows naturlay progression of previewing a selection then changing it.
+                                # already coded
+                                # takes args more cleanly if range is explicit.  
+
+- flip, del, edit, in-de dent
 =end
