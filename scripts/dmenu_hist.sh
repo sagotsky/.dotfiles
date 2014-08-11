49005069582967dc49a95b85de14d324d4b431ef
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-08-11 16:52:10 -0400

    dmenu_hist can do slmenu instead if symlinked and renamed.  sneaky.

--- scripts/dmenu_hist.sh
+++ scripts/dmenu_hist.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 file=$1
+menu_cmd='dmenu'
+[[ "$(basename $0)" == 'slmenu_hist.sh' ]] && menu_cmd='slmenu'  # symlink to this file to use slmenu instead of dmenu
 histdir="$HOME/.dmenu-hist/"
 bucket="$histdir/$file"
 shift
@@ -10,7 +12,7 @@ if [ ! -d "$histdir" ] ; then mkdir "$histdir" ; fi
 if [ ! -f "$bucket" ] ; then touch "$bucket" ; fi
 
 IFS=$'\n' stdin="$(cat)"
-sel=$(echo -e "$stdin\n$(grep . $bucket)" | sort | uniq -c | sort -bnr | sed -e 's/.*[0-9]\+ //' | dmenu $@)
+sel=$(echo -e "$stdin\n$(grep . $bucket)" | sort | uniq -c | sort -bnr | sed -e 's/.*[0-9]\+ //' | $menu_cmd $@)
 
 if [ "$?" -eq "0" ] ; then 
   echo "$sel" >> "$bucket"
---
+++ scripts/slmenu_hist.sh
@@ -0,0 +1,1 @@
+/home/sagotsky/scripts/dmenu_hist.sh
