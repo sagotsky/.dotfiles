490a34bdff8a0bfdc01a38247d0131b27f7b3e13
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-10-10 10:22:50 -0400

    Easy launcher for named tmux session, with persistent ssh prot forward

---
+++ scripts/tmux-session.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "$1" == '-h' || "$1" == '--help' ]] ; then
+  echo tmux-session.sh session_name prog1 prog2 ... progN
+  exit 1
+fi
+SESSION=$1
+shift
+
+# Runs a script in tmux or attaches to existing
+tmux ls | grep -q "$SESSION" || 
+  tmux -2 new-session -d -s "$SESSION" $1
+  shift
+
+  while [[ "$#" -gt "0" ]] ; do 
+    NTH="$(( NTH + 1 ))"
+    tmux new-window -t "$SESSION:$NTH" $1
+    shift
+  done
+
+    # eval $(keychain --eval)
+    # screen-ssh-tunnel.sh
