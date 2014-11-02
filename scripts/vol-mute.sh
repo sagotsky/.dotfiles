975e50f8ae606c74bb50e28839f5c0c1511cbb08
Jon Sagotsky <sagotsky@gmail.com>
2014-11-02 06:33:57 -0500

    Pulse fix for my edge case.  Touch dev files so inotify sees volume change.

--- scripts/vol-mute.sh
+++ scripts/vol-mute.sh
@@ -2,6 +2,8 @@
 
 amixer -D default sset Master,0 toggle  > /dev/null
 
+touch /dev/snd/controlC0 
+
 #amixer -D default sget Master,0 | grep dB | head -n1 | cut -f 7 -d " " | sed -e's/\[//g' -e 's/\]//g' > /home/sagotsky/.volinfo
 
 # if mixer is off (muted) indicate it in volinfo
--- scripts/vol-up.sh
+++ scripts/vol-up.sh
@@ -10,5 +10,6 @@
 
 #amixer -D default sset PCM,0 100% unmute > /dev/null
 amixer -D default sset Master,0 4%+ unmute > /dev/null
+touch /dev/snd/controlC0 
 #amixer -D default sget Master,0 | grep dB | head -n 1 | cut -f 7 -d " "| sed -e's/\[//g' -e 's/\]//g' > /home/sagotsky/.volinfo
 
