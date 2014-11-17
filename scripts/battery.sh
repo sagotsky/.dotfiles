f9ab6acf4d94624b801f075e604069ea63c5ce23
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-11-16 21:59:02 -0500

    New colors for new xmobarc

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -12,12 +12,13 @@ if [ $PWR -gt 20 ] ; then
 fi
 
 if [ $PWR -gt 60 ] ; then
-  COLOR='green'
+  COLOR='grey'
 fi
 
 if [[ $(acpi -a) == 'Adapter 0: on-line' ]] ; then
-  COLOR='cadetblue'
-  PWR='AC'
+  #COLOR='cadetblue'
+  #PWR='AC'
+  PWR=''
   SYM=''
 fi
 
