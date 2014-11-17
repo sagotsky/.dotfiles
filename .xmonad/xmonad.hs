79309b22232eb66089668d674f624eed2bfa7601
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-11-16 21:55:22 -0500

    And a keybinding for new work dir stuff

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -143,8 +143,7 @@ myKeys = [
     , ("M-C-<Return>",     spawn "urxvt") -- urxvt alternate term is transparent
     , ("M-s",              spawn "HOST=$(grep '^host ' .ssh/config | cut -f 2 -d' ' | ~/scripts/dmenu_hist.sh ssh -l 10 -i -s 0 -sb '#cfb000' -sf '#000' -nf '#fff' -nb '#4a525a' -fn -*-terminus-bold-r-*-*-16) && xterm -e 'ssh $HOST' ")  
     , ("M-S-s",            spawn "xterm -e stage_ssh.sh $(stage_ssh.sh | dmenu -l 10 -i -s 0 -fn terminus-bold-16 )")  
-    , ("M-v",              spawn "gvim -c 'cd ~/repos/plm' -c pwd") 
-    , ("M-S-v",            spawn "gvim -c 'cd ~/repos/open-pro' -c pwd") 
+    , ("M-v",              spawn "working-dir.sh") 
 
 
     -- music
@@ -203,7 +202,7 @@ myKeys = [
     , ("M-S-q", return ()) -- don't you fucking quit.  that's what ctrl-alt-backsp is for.
 
     -- misc scripts
-    , ("M-/",   spawn "zeal -f") -- rails docs menu
+    , ("M-/",   spawn "killall -9 zeal ; zeal -f") -- rails docs menu
     , ("M-y",   spawn "cli-board.sh") -- copies text into clip board
     , ("M-S-y", spawn "cheat-sheet.sh") -- views files in .cheat-sheets
 	]
