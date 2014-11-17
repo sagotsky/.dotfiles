609136e76451310140730b297be3827b79ef3b30
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-11-16 21:58:53 -0500

    always use vpn

--- scripts/ssh_url.sh
+++ scripts/ssh_url.sh
@@ -3,6 +3,8 @@
 # handle ssh:// urls from firefox
 #gconftool-2 -s /desktop/gnome/url-handlers/ssh/enabled --type Boolean false
 
+[[ "$(hostname)" == "boxy" ]] && nmcli c up 'VPN patientslikeme'
+
 if [[ "$@" =~ 'ssh://' ]] ; then
   xterm -bg '#223' -e "ssh  -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ${@##ssh://}"
 fi
