f4bb12a0c2b03aff42e9717152262dd3939a6dd0
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-11-16 21:58:33 -0500

    always use vpn

--- scripts/nm-select-network.sh
+++ scripts/nm-select-network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-CONNECTED="$(nm-tool | grep '  State: *connected' -B 3 | head -n 1 | sed -e 's/.*\[\(.*\)\].*/\1/')"
+CONNECTED="$(nmcli d | grep connected | awk '{print $4}')"
 ID=$( (echo $CONNECTED ; nmcli -f name -t c) | sort | uniq -c | sort -nr | cut -f8- -d' ' | dmenu -i -p "$CONNECTED" -l 6 -b)
 
 [[ $? && "$ID" != "" ]] && nmcli c up id "$ID"
