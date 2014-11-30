292f5c596a09db77c9649b75489201cb6956e372
Jon Sagotsky <jsagotsky@patientslikeme.com>
2014-11-29 20:51:41 -0500

    Battery life icons.  Going overboard on the xbms.

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -5,20 +5,21 @@ if [[ $(acpi -a) == 'Adapter 0: on-line' ]] ; then
   exit 
 fi 
 
-PWR="$(acpi -b | cut -f4 -d' ' | tr -d '%' | tr -d ',')"
+PWR="$(acpi -b | cut -f4 -d' ' | tr -d '?' | tr -d ',')"
 [[ "x$PWR" == "x" ]] && exit
 #https://github.com/zoresvit/dotfiles/tree/master/xmonad/.xmonad/images
 
 case "$PWR" in
-  [7-9]* ) COLOR='dimgrey' ;;
-  [4-6]* ) COLOR='lightgoldenrod' ;;
-  [2-3]* ) COLOR='yellow' ;;
-  * )      COLOR='red' ;;
+
+  [7-9]*% ) COLOR='dimgrey' ICON='battery-full';;
+  [4-6]*% ) COLOR='lightgoldenrod' ICON='battery-med';;
+  [2-3]*% ) COLOR='yellow' ICON='battery-low';;
+  * )       COLOR='red' ICON='battery-empty';;
 esac
 
-ICON='<icon=/usr/share/dzen2/bitmaps/battery.xbm/>'
-SYM='%'
+#ICON='<icon=/usr/share/dzen2/bitmaps/battery.xbm/>'
+ICON="<icon=$ICON.xbm/>"
 
-echo "<fc=$COLOR>$ICON $PWR$SYM</fc> "
+echo "<fc=$COLOR>$ICON$PWR</fc> "
 
 
