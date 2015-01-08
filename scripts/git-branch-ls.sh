942b73aac473fedebcb1c1fab3a909e055749ffc
Jon Sagotsky <jsagotsky@patientslikeme.com>
2015-01-08 10:34:19 -0500

    `git-branch-ls.sh` shows all changed files in current branch

---
+++ scripts/git-branch-ls.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# git merge-base master 
+
+function current_branch() {
+  git branch | grep '*' | cut -f2 -d' '
+}
+
+function branches_first_commit() {
+  git merge-base master $1
+}
+
+BRANCH="${1:-`current_branch`}"
+git log "$(branches_first_commit $BRANCH)"..HEAD --name-only --pretty=format: | sort | uniq
+
+
+
