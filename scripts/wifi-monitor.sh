582483c8c543789e8021c8d0e504144a5e2e2e24
Jon Sagotsky <sagotsky@gmail.com>
2015-01-09 15:17:37 -0500

    Wifi monitor in xmobar.  Only show signal if it's low.

---
+++ scripts/wifi-monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+function signal() {
+  grep wlan0 /proc/net/wireless | awk '{print $3*100/70}' | cut -f 1 -d'.'
+}
+
+function essid() {
+  nmcli d | grep connected | grep wlan0 | awk '{print $4}'
+}
+
+[[ "$(signal)" -lt 60 ]] && echo "<fc=red><icon=wifi.xbm/> $(signal)%</fc>" || echo ''
+
