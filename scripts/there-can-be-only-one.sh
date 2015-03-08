72aa5130d6175c4b53f4003327cfee5185c78e38
Jon Sagotsky <sagotsky@gmail.com>
2015-03-07 22:10:08 -0500

    new file for preventing duplicates

---
+++ scripts/there-can-be-only-one.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# Find the process that spawned this one.  Kill all 
+# other instances of it.  Intended for processes that
+# are started automatically, but only need one instance.
+
+PCMD="$(ps -ocommand= -p $PPID)"
+pidof -x "${PCMD##*/}" -o "$PPID" | xargs kill -9
