9f0817e05b74b726e5967dd8f1e04c1bb62d9234
Jon Sagotsky <sagotsky@gmail.com>
2015-07-14 10:19:45 -0400

    volume controls were too specific

--- scripts/vol-mute.sh
+++ scripts/vol-mute.sh
@@ -1,12 +1,5 @@
 #!/bin/sh 
 
-amixer -c 0 sset Master,0 toggle  > /dev/null
+amixer sset Master  toggle  > /dev/null
 
 touch /dev/snd/controlC0 
-
-#amixer -D default sget Master,0 | grep dB | head -n1 | cut -f 7 -d " " | sed -e's/\[//g' -e 's/\]//g' > /home/sagotsky/.volinfo
-
-# if mixer is off (muted) indicate it in volinfo
-#if [ $(amixer -D default sget Master,0 | grep off | wc -c) -gt 0 ] ; then
-    #echo "--%" > /home/sagotsky/.volinfo
-#fi
