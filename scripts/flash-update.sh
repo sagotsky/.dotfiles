c4a1695dbd61ace2f9729929bf6e7a5cc87c2065
Jon Sagotsky <sagotsky@gmail.com>
2016-08-19 20:54:29 -0400

    Updated flash player file names

--- scripts/flash-update.sh
+++ scripts/flash-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FLASH_URL="https://get.adobe.com/flashplayer/download/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_64-bit&standalone=1"
+FLASH_URL="https://get.adobe.com/flashplayer/download/?installer=FP_22.0_for_other_Linux_64-bit_(.tar.gz)-_PPAPI&standalone=1"
 TMP="$(mktemp -d)"
 
 function tarball_url() {
@@ -14,11 +15,11 @@ function download_and_unpack() {
 }
 
 function patch_fullscreen() {
-  sed -i s/_NET_ACTIVE_WINDOW/_ZZZ_ACTIVE_WINDOW/ "$TMP/libflashplayer.so"
+  sed -i s/_NET_ACTIVE_WINDOW/_ZZZ_ACTIVE_WINDOW/ "$TMP/libpepflashplayer.so"
 }
 
 function install_flash() {
-  sudo mv "$TMP/libflashplayer.so" /usr/lib/mozilla/plugins/
+  sudo mv "$TMP/libpepflashplayer.so" /usr/lib/mozilla/plugins/
 }
 
 echo "Downloading flash..."
