5b09c0f10b5730d324e15e43d40514d333ecdd5e
Jon Sagotsky <sagotsky@gmail.com>
2016-11-21 14:16:31 -0500

    Change available frequencies.  Noop on ctrl-c.

--- scripts/cpu-scale-dmenu.sh
+++ scripts/cpu-scale-dmenu.sh
@@ -7,14 +7,14 @@ function current() {
     sed -e 's/current CPU frequency is //'
 }
 
-FREQS="$(echo {3.8,3.5,3.0,2.5,2.0,1.5,1.0}ghz)"
+FREQS="$(echo {4,3,2,1}.0ghz)"
 FREQ="$(
   echo "$FREQS" | xargs -n 1 echo |
   dmenu -s 0 -p "$(current)"
 )"
 
 # passing stuff to sudo seems bad here.  won't work with dmenu.  need to check if the input from dmenu is in array it started with.
-if [[ $FREQS == *"$FREQ"* ]] ; then
+if [[ "$?" == "0" && $FREQS == *"$FREQ"* ]] ; then
   \sudo cpufreq-set --max $FREQ 
   sleep 0.8
   notify-send CPU "$(current)" -u low
