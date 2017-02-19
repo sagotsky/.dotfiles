219bd093a22e2620726da6fb7e845d6d4367ecf2
Jon Sagotsky <sagotsky@gmail.com>
2017-02-19 14:26:32 -0500

    wifi monitor doesn't care about device name

--- home/scripts/wifi-monitor.sh
+++ home/scripts/wifi-monitor.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# WIFI=wlan
-WIFI="wlp4s0"
-
 function signal() {
-  grep $WIFI /proc/net/wireless | awk '{print $3*100/70}' | cut -f 1 -d'.'
+  tail -n 1 /proc/net/wireless | awk '{print $3*100/70}' | cut -f 1 -d'.'
 }
 
 function essid() {
-  iwconfig 2>/dev/null | grep $WIFI | cut -f 2 -d\"
+  iwconfig 2>/dev/null | grep ESSID | cut -f 2 -d\"
 }
 
 [[ "$(signal)" -lt 60 ]] && echo "<fc=red><icon=wifi.xbm/> $(signal)%</fc>" || echo ''
