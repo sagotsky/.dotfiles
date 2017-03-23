d5c381c0626106feb8610d5a3094efa90c3b6177
Jon Sagotsky <sagotsky@gmail.com>
2017-03-23 16:17:18 -0400

    A million little changes

--- home/scripts/bender-monitors.sh
+++ home/scripts/bender-monitors.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# should this run before or after xinitrc exports scaling vars?
+
 EXTERN="DP-3.2"
-EXTERN_SCALE="2x2"
+# EXTERN_SCALE="2x2" # headache!
+EXTERN_SCALE="1x1"
 EXTERN_ON='auto'
 
 INTERN="DP-4"
@@ -17,6 +20,10 @@ case $MODE in
     # todo: find a better resolution or disable scaling.
     INTERN_POS=''
     ;;
+  startx)
+    # initialize with this scaling, then scale back to default
+    EXTERN_SCALE='2x2'
+    ;;
 esac
 
 xrandr \
@@ -30,4 +37,5 @@ xrandr \
     --scale $EXTERN_SCALE
 
 
+nitrogen --restore
 xbacklight -set 75
