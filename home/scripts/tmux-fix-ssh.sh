46e565dcbcf0d4034c8186758d9ee4e8b23f2a1d
Jon Sagotsky <sagotsky@gmail.com>
2017-12-29 09:44:00 -0500

    script to set env vars for ssh in existing tmux sessions

---
+++ home/scripts/tmux-fix-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# add ssh agent env vars to all tmux sessions
+
+tmux ls -F'#S' | while read session_name ; do
+  tmux set-environment -t $session_name SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  tmux set-environment -t $session_name SSH_AGENT_PID $SSH_AGENT_PID
+done
+
