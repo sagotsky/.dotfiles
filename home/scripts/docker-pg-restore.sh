d96664142031ca036fa6500cd96c22c8962659db
Jon Sagotsky <sagotsky@gmail.com>
2019-01-23 13:26:57 -0500

    Finally got a pg restore that works without tinkering

--- home/scripts/docker-pg-restore.sh
+++ home/scripts/docker-pg-restore.sh
@@ -5,9 +5,9 @@
 db="$(ls ~/db_dumps/plm_development*.pgdump* --sort=time | head -n 1)"
 echo Loading from $db
 
-# docker exec -i docker_postgres_1 dropdb -Upostgres plm_sanitizer_work --if-exists
+docker exec -i docker_postgres_1 dropdb -Upostgres plm_sanitizer_work --if-exists
 docker exec -i docker_postgres_1 dropdb -Upostgres plm_development --if-exists
-# docker exec -i docker_postgres_1 createdb -Upostgres plm_sanitizer_work
+docker exec -i docker_postgres_1 createdb -Upostgres plm_sanitizer_work
 docker exec -i docker_postgres_1 createdb -Upostgres plm_development
 docker exec -i docker_postgres_1 pg_restore -e -C -c --no-acl --no-owner -Upostgres -d plm_development < "$db"
 docker exec -i docker_postgres_1 dropdb -Upostgres plm_development --if-exists
