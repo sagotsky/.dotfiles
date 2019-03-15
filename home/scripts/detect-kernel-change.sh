06d20e598a0877d8040017afb9e52697fb5d0dca
Jon Sagotsky <sagotsky@gmail.com>
2019-03-15 13:50:09 -0400

    Fix kernel change detection script

--- home/scripts/detect-kernel-change.sh
+++ home/scripts/detect-kernel-change.sh
@@ -2,7 +2,7 @@
 
 # Provides a status icon for xmobar so I know when I've changed my kernel and should reoobt
 
-if pacman -Si linux-lts linux | grep "$(uname -r | sed -e 's/-lts//')" &>/dev/null ; then
+if pacman -Si linux-lts linux | grep "$(uname -r | sed -e 's/-lts//' -e 's/-ARCH//' -e 's/-arch/.arch/')" &>/dev/null ; then
   echo ''
 else
   echo " KERNEL CHANGED " #&& exit 1
