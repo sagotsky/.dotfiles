b39e125b49d95266027e59f9060d3b02a7409bf5
Jon Sagotsky <sagotsky@gmail.com>
2019-05-02 21:44:47 -0400

    Monitor and font settings in polybar

--- home/scripts/init-polybar.sh
+++ home/scripts/init-polybar.sh
@@ -10,8 +10,13 @@ rm $FIFO &>/dev/null
 mkfifo $FIFO
 
 export PRIMARY_MONITOR="$(xrandr -q | grep primary | cut -f1 -d' ')" 
+if [[ "$PRIMARY_MONITOR" == "DP-4" ]] ; then
+  export FONT_SIZE=18
+else
+  export FONT_SIZE=10
+fi
 
-if grep $(hostname) ~/.config/polybar/config &> /dev/null ; then
+if grep "$(hostname)" ~/.config/polybar/config &> /dev/null ; then
   BAR="$(hostname)"
 else
   BAR="example"
