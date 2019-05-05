5cabf8178495188a94ced18465037a3d9c2bf20d
Jon Sagotsky <sagotsky@gmail.com>
2019-05-04 21:13:27 -0400

    Gave polybar a 4k and 1k setting for this laptop

--- home/scripts/init-polybar.sh
+++ home/scripts/init-polybar.sh
@@ -9,17 +9,23 @@ FIFO='/tmp/.polybar.fifo'
 rm $FIFO &>/dev/null
 mkfifo $FIFO
 
-export PRIMARY_MONITOR="$(xrandr -q | grep primary | cut -f1 -d' ')" 
-if [[ "$PRIMARY_MONITOR" == "DP-4" ]] ; then
-  export FONT_SIZE=18
-else
-  export FONT_SIZE=10
-fi
 
 if grep "$(hostname)" ~/.config/polybar/config &> /dev/null ; then
   BAR="$(hostname)"
 else
   BAR="example"
+fi
+
+PRIMARY_MONITOR="$(xrandr -q | grep primary | cut -f1 -d' ')" 
+export PRIMARY_MONITOR
+
+# scale up for the 4k laptop
+if [[ "$BAR" == "bender" ]] ; then
+  if [[ "$PRIMARY_MONITOR" == "DP-4" ]] ; then
+    export BAR="$BAR-4k"
+  else
+    export BAR="$BAR-4k"
+  fi
 fi
 
 polybar $BAR &
