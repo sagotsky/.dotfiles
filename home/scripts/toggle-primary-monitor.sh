119d83b21a6fc5fdb9be503e731011fe47dcc1d3
Jon Sagotsky <sagotsky@gmail.com>
2019-05-17 20:09:53 -0400

    Script toggles which monitor is primary and restarts xmonad

---
+++ home/scripts/toggle-primary-monitor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# works for getting off secondary.  how about reconnecting?
+NEXT_PRIMARY=$(xrandr -q | grep ' connected' | grep -v 'primary' | cut -f 1 -d' ')
+if [[ "$NEXT_PRIMARY" == "" ]] ; then
+  # just use the connected screen.  it can stay primary.
+  NEXT_PRIMARY=$(xrandr -q | grep ' connected' |  cut -f 1 -d' ')
+fi
+
+# will this drop the other screen?  or just switch primary?
+xrandr --output $NEXT_PRIMARY --primary
+xmonad --restart
+
