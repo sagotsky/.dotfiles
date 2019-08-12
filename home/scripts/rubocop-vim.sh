0a85f2e4658ae7cc317f8179a94a7ead7ad904d5
Jon Sagotsky <sagotsky@gmail.com>
2019-08-12 16:02:46 -0400

    Let's try having vim/ale update rubocop instead of silently failing

---
+++ home/scripts/rubocop-vim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# runs rubocop on behalf of vim
+# will try to update bundler if necessary since ale will silently drop warnings if we don't have the gem
+
+# BUNDLE_CMD="bundle"
+# if [[ "$DOCKER_SPECS" != "" ]] ; then
+# need all gems to be local?
+#   BUNDLE_CMD="docker/run bundle"
+# fi
+
+if [[ -f "bin/rubocop" ]] ; then
+  bin/rubocop $@
+  [[ "$?" == "2" ]] && bundle exec gem install && bin/rubocop $@
+else
+  rubocop $@
+fi
