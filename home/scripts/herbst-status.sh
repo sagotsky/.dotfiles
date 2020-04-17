c8b305323fde5e1c635f40dc5a7036398a768f19
Jon Sagotsky <sagotsky@gmail.com>
2020-04-17 18:51:45 -0400

    herbst-status urgent color

--- home/scripts/herbst-status.sh
+++ home/scripts/herbst-status.sh
@@ -3,6 +3,7 @@
 SELECTED_WINDOW_FG='#aaaaaa'
 ACTIVE_WINDOW_FG='#777777'
 EMPTY_WINDOW_FG='#555555'
+URGENT_WINDOW_FG='#ff5500'
 
 ps ax |
   grep "$(basename $0)" |
@@ -28,18 +29,12 @@ polybar_fg() {
 
 
 function format_window() {
-  # icon="$(echo $1 | sed -e 's/[#\.:]/ /g')"
   icon="⚫"
-  # slice out first char?  eliminating * may help
-  # case $1 in
-  #   :*)  color $ACTIVE_WINDOW_FG "$icon" ;;
-  #   \#*) color $SELECTED_WINDOW_FG "$icon" ;;
-  #   .*)  color $EMPTY_WINDOW_FG "$icon" ;;
-  # esac
   case ${1:0:1} in # first char of first arg
     ":") color $ACTIVE_WINDOW_FG "$icon" ;;
     "#") color $SELECTED_WINDOW_FG "$icon" ;;
     ".") color $EMPTY_WINDOW_FG "$icon" ;;
+    "!") color $URGENT_WINDOW_FG "$icon" ;;
   esac
 }
 
