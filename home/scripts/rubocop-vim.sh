123a3bd59d370a99bbcf7970343872746300f5aa
Jon Sagotsky <sagotsky@gmail.com>
2020-04-21 13:49:02 -0400

    Playing with rubocop launcher.  Docker is making this hard.  I don't want to rely on system docker.  But running it in docker from vim's launcher is convoluted

--- home/scripts/rubocop-vim.sh
+++ home/scripts/rubocop-vim.sh
@@ -8,10 +8,21 @@
 # need all gems to be local?
 #   BUNDLE_CMD="docker/run bundle"
 # fi
+# if [[ -f "bin/rubocop" ]] ; then
+#   docker/run bin/rubocop -a $@
+#   [[ "$?" != "0" ]] && docker/run bundle exec gem install rubocop rubocop  && docker/run bin/rubocop -a $@
+# else
+#   rubocop -a $@
+# fi
 
-if [[ -f "bin/rubocop" ]] ; then
-  bin/rubocop -a $@
-  [[ "$?" != "0" ]] && bundle exec gem install rubocop rubocop  && bin/rubocop -a $@
+# ez-specific.
+if [[ -f "docker/run" ]] ; then
+  cmd="docker-compose exec -T ez-rails-web sh -c bin/rubocop $@"
+  # export COMPOSE_INTERACTIVE_NO_CLI=0
+  echo $cmd
+  `$cmd`
+  # COMPOSE_INTERACTIVE_NO_CLI=0 docker-compose exec -T ez-rails-web sh -c "bin/rubocop " $@ " "
+  # can we keep the args?  -a and friends are stripped
 else
   rubocop -a $@
 fi
