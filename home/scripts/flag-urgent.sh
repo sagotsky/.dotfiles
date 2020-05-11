04bca303c19bdc29bd2cae65ea3a4e0df1bcbe5c
Jon Sagotsky <sagotsky@gmail.com>
2020-05-11 10:06:00 -0400

    Flagging urgency hook with wmctrl.  It's more easily inspected by xprop this way.

--- home/scripts/flag-urgent.sh
+++ home/scripts/flag-urgent.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-app=$1
-xwininfo -root -tree | grep $app | awk '{print $1}' | while read id ; do
-  xdotool set_window --urgency 0 $id
-  sleep 0.05
-  xdotool set_window --urgency 1 $id
-done
+wmctrl -r "$@" -b add,demands_attention
+
+# app=$1
+# xwininfo -root -tree | grep $app | awk '{print $1}' | while read id ; do
+#   xdotool set_window --urgency 0 $id
+#   sleep 0.05
+#   xdotool set_window --urgency 1 $id
+# done
