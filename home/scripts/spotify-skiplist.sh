be9f84b02d0811c12425c27466781d38fa40775e
Jon Sagotsky <sagotsky@gmail.com>
2020-09-18 10:31:29 -0400

    Small daemon to auto skip in spotify.
    Spotify kept giving me christian rock.  They weren't giving me
    Nickelback, but you can never be too safe.

---
+++ home/scripts/spotify-skiplist.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+CONFIG="$HOME/.spotify-skiplist"
+
+check_dependencies() {
+   which playerctl &>/dev/null || abort "You must install playerctl"
+}
+
+check_config() {
+  [[ -f ~/.spotify-skiplist ]] || abort "$CONFIG does not exist"
+}
+
+abort() {
+  echo $@
+  exit 1
+}
+
+usage() {
+  echo "Run spotify-skiplist.sh as a daemon.  Artists in $CONFIG will automatically be skipped."
+  echo "E.g. \`echo Nickelback >> $CONFIG\`"
+}
+
+if [[ "$#" != "0" ]] ; then
+   usage
+   exit
+fi
+
+check_dependencies
+check_config
+
+playerctl metadata -f '{{ artist }}' --follow 2>/dev/null | while read artist ; do
+  if grep "${artist}" $CONFIG ; then
+    echo "Skipping ${artist}"
+    playerctl next
+  fi
+done
