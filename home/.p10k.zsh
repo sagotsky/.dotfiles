e46d4e073bd2d3612202bd749adc3c31035aa7a7
Jon Sagotsky <sagotsky@gmail.com>
2020-09-28 16:38:57 -0400

    zsh prompt shouldn't truncate git branches

--- home/.p10k.zsh
+++ home/.p10k.zsh
@@ -411,7 +411,8 @@
 
     # If local branch name or tag is at most 32 characters long, show it in full.
     # Otherwise show the first 12 … the last 12.
-    (( $#where > 32 )) && where[13,-13]="…"
+    # (( $#where > 32 )) && where[13,-13]="…"
+    (( $#where > 64 )) && where[24,-24]="…"
     res+="${clean}${where//\%/%%}"  # escape %
 
     # Display the current Git commit if there is no branch or tag.
