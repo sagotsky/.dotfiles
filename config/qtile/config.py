fcb55521d118bca044a66c1a2ccf649740afc87b
Jon Sagotsky <sagotsky@gmail.com>
2021-03-14 14:00:26 -0400

    Trying shadows over borders for focus

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -114,7 +114,9 @@ layouts = [
         border_normal="#222233",
         single_border_width=0,
         name="Tall",
-        border_width=1
+        border_width=0,
+        margin = 8,
+        single_margin = 0
     ),
     layout.Max(),
     # layout.Stack(num_stacks=2),
