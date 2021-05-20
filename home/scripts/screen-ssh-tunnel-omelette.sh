a3761bd72e0fd1aa8d5ff775488972916eea8a75
Jon Sagotsky <jon.sagotsky@ezcater.com>
2021-05-20 16:45:02 -0400

    Tweak the tunnel script

--- home/scripts/screen-ssh-tunnel-omelette.sh
+++ home/scripts/screen-ssh-tunnel-omelette.sh
@@ -13,12 +13,12 @@ TUNNELS='-R 34567:127.0.0.1:22 -R 3000:127.0.0.1:3000 -R 4000:127.0.0.1:4000 -R 
 OPTIONS='-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o BatchMode=yes'
 AUTOSSH_MAXSTART=2
 
-while [[ true ]] ; do
+while which autossh ; do
   eval $(keychain -q --eval)
   date
   # 8080 is NATed to rj.  if it curls, we're up.  hopefully this will bypass the spam detector.
   #curl -s robotjesus.net:8080 && autossh $TUNNELS $OPTIONS rj
-  autossh -M 55555 $TUNNELS $OPTIONS rj
+  autossh -M 0 $TUNNELS $OPTIONS rj
   sleep 1m
 done
 
