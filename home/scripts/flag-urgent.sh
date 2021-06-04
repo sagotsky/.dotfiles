956b9f344200af287748f074c6e3777a5f0586d7
Jon Sagotsky <jon.sagotsky@ezcater.com>
2021-06-04 10:38:09 -0400

    Use ICCCM urgency hint instead of EWMH needs attention.

--- home/scripts/flag-urgent.sh
+++ home/scripts/flag-urgent.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-wmctrl -r "$@" -b remove,demands_attention
-sleep 0.1
-wmctrl -r "$@" -b add,demands_attention
+# wmctrl -r "$@" -b remove,demands_attention
+# sleep 0.1
+# wmctrl -r "$@" -b add,demands_attention
 
-# app=$1
-# xwininfo -root -tree | grep $app | awk '{print $1}' | while read id ; do
-#   xdotool set_window --urgency 0 $id
-#   sleep 0.05
-#   xdotool set_window --urgency 1 $id
-# done
+app=$1
+xwininfo -root -tree | grep $app | awk '{print $1}' | while read id ; do
+  xdotool set_window --urgency 0 $id
+  sleep 0.05
+  xdotool set_window --urgency 1 $id
+done
