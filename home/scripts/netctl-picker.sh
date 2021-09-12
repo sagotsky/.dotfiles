7442c5a3fd846f9df80949653e2af3f4ba005d76
Jon Sagotsky <sagotsky@gmail.com>
2021-09-12 13:53:49 -0400

    netctl script doesn't disconnect if aborted

--- home/scripts/netctl-picker.sh
+++ home/scripts/netctl-picker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wap="$(netctl list | dmenu | tr -d '*')"
-if [[ "$?" != "0" ]] ; then exit 1 ; fi
+if [[ "$wap" == "" ]] ; then exit 1 ; fi
 
 sudo /usr/bin/netctl stop-all
 sudo /usr/bin/netctl start $wap
