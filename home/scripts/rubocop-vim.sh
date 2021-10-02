4ae2b5841a6ec69cd12c0421f50e35d20e442a64
Jon Sagotsky <sagotsky@gmail.com>
2021-10-02 14:21:58 -0400

    rubocop script cant assume binstub

--- home/scripts/rubocop-vim.sh
+++ home/scripts/rubocop-vim.sh
@@ -3,5 +3,13 @@
 # runs rubocop on behalf of vim
 # will try to update bundler if necessary since ale will silently drop warnings if we don't have the gem
 
-(bin/rubocop -v || notify-send bundle "updating rubocop" && bundle install )&>/dev/null
-bin/rubocop "$@"
+rubocop() {
+    if [ -f bin/rubocop ] ; then
+        bin/rubocop "$@"
+    else
+        bundle exec rubocop "$@"
+    fi
+}
+
+(rubocop -v || notify-send -u critical bundle "updating rubocop" && bundle install )&>/dev/null
+rubocop "$@"
