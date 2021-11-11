b1bb16128fc2af495e5b1b0a4e7dc2f231820279
Jon Sagotsky <sagotsky@gmail.com>
2021-11-11 16:35:31 -0500

    rubocop script adds overridable config

--- home/scripts/rubocop-vim.sh
+++ home/scripts/rubocop-vim.sh
@@ -3,11 +3,21 @@
 # runs rubocop on behalf of vim
 # will try to update bundler if necessary since ale will silently drop warnings if we don't have the gem
 
+opts() {
+    override_yml="./.rubocop.override.yml"
+    if [ -f "$override_yml" ] ; then
+        echo "--config $override_yml"
+    else
+        echo ""
+    fi
+}
+
+
 rubocop() {
     if [ -f bin/rubocop ] ; then
-        bin/rubocop "$@"
+        bin/rubocop "$@" `opts`
     else
-        bundle exec rubocop "$@"
+        bundle exec rubocop "$@" `opts`
     fi
 }
 
