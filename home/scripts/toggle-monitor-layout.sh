e75fa86c8e262050ff36de2534ef30462ca11418
Jon Sagotsky <sagotsky@gmail.com>
2021-12-05 12:51:43 -0500

    monitor toggle hits xmodmap, wallpaper

--- home/scripts/toggle-monitor-layout.sh
+++ home/scripts/toggle-monitor-layout.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-xmodmap ~/.xmodmap
 least_recently_used_layout="$( ls -t ~/.screenlayout/*.sh | tail -n 1 )"
 notify-send "layout:" "$least_recently_used_layout"
 
 $least_recently_used_layout
 touch -m $least_recently_used_layout
+sleep 0.1
+
+xmodmap ~/.xmodmap
 killall yambar
 yambar &
 [ -f ~/.xsession.d/picom.sh ] && ~/.xsession.d/picom.sh &
+nitrogen --restore
+setxkbmap -option terminate:ctrl_alt_bksp
