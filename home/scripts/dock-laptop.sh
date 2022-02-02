76848380f5833f618124e0b2a6a8aa6f7eead894
Jon Sagotsky <sagotsky@gmail.com>
2022-02-02 13:00:22 -0500

    Fix up docking script
    xrandr -q was slow.  Instead, lets just set the screen we want and
    fallback if it's missing.

--- home/scripts/dock-laptop.sh
+++ home/scripts/dock-laptop.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+external_screen="DP-1-1.1"
+internal_screen="eDP-1"
+
 ~/.screenlayout/vertical-2.sh
 xmodmap ~/.xmodmap
-killall yambar ; yambar
+
+# `xrandr -q` lags for 3s.  this tries to skip the query by offering a fallback.
+xrandr --output "$external_screen" --primary ||                     # try external
+    xrandr --output "$internal_screen" --primary       # fallback to laptop
+
+killall yambar ; yambar &
