452a987c8ea6061d0df3a2b9f7562ffe69aaa9b2
Jon Sagotsky <sagotsky@gmail.com>
2022-03-04 10:12:13 -0500

    Dock script minor tweaks

--- home/scripts/dock-laptop.sh
+++ home/scripts/dock-laptop.sh
@@ -7,9 +7,14 @@ notify-send "docking" -h string:fgcolor:#ffffff -h string:bgcolor:#00b373
 
 ~/.screenlayout/vertical-2.sh
 xmodmap ~/.xmodmap
+killall yambar
+killall xcompmgr &>/dev/null
 
 # `xrandr -q` lags for 3s.  this tries to skip the query by offering a fallback.
 xrandr --output "$external_screen" --primary ||                     # try external
     xrandr --output "$internal_screen" --primary       # fallback to laptop
 
-killall yambar ; yambar &
+yambar &
+caps-unlock.sh # just in case I hit caps before xmodmap
+nitrogen --restore
+~/.xsession.d/xcompmgr &>/dev/null &
