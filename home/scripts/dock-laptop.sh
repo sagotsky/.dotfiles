68869c416bf33a4a9e26ea0152e0b5fdfd7174f2
Jon Sagotsky <sagotsky@gmail.com>
2022-06-01 15:50:51 -0400

    docking script fix

--- home/scripts/dock-laptop.sh
+++ home/scripts/dock-laptop.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-external_screen="DP-1-1.1"
-internal_screen="eDP-1"
+# external_screen="DP-1-1.1"
+# internal_screen="eDP-1"
 
 notify-send "docking" -h string:fgcolor:#ffffff -h string:bgcolor:#00b373
 
-~/.screenlayout/vertical-2.sh
+~/.screenlayout/vertical-2.sh || ~/.screenlayout/single.sh
+
 xmodmap ~/.xmodmap
 killall yambar
 killall xcompmgr &>/dev/null
 killall trayer
 
 # `xrandr -q` lags for 3s.  this tries to skip the query by offering a fallback.
-xrandr --output "$external_screen" --primary ||                     # try external
-    xrandr --output "$internal_screen" --primary       # fallback to laptop
+# xrandr --output "$external_screen" --primary ||                     # try external
+#     xrandr --output "$internal_screen" --primary       # fallback to laptop
 
 dkcmd restart
 xbacklight-50
