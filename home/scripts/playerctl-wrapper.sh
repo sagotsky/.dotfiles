992b7c0eecc55d451eba1ede931832269d49cde5
Jon Sagotsky <sagotsky@gmail.com>
2023-08-02 14:23:56 -0400

    try to restart playerctl when disconnected

--- home/scripts/playerctl-wrapper.sh
+++ home/scripts/playerctl-wrapper.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 killall playerctl &>/dev/null
 
-playerctl -p spotify metadata -f '{{ status }}{{ title }}' --follow 2>/dev/null | while read status ; do
-  case $status in
-    Playing*)
-      playerctl metadata -f "{{ artist }} - {{ title }}"
-      ;;
-    Paused*)
-      echo "-"
-      ;;
-    **)
-      echo ""
-      ;;
-  esac
+
+# todo: fix when pulse is killed
+while which playerctl &>/dev/null ; do
+  playerctl -p spotify metadata -f '{{ status }}{{ title }}' --follow 2>/dev/null | while read status ; do
+    case $status in
+      Playing*)
+        playerctl metadata -f "{{ artist }} - {{ title }}"
+        ;;
+      Paused*)
+        echo "-"
+        ;;
+      **)
+        echo ""
+        ;;
+    esac
+  done
+  sleep 10
 done
