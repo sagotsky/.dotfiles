6cd59580bb6d0a8cd3bea12de912ed1deb34c77b
Jon Sagotsky <sagotsky@gmail.com>
2024-10-24 09:25:47 -0400

    linux laptop dock script is less opinionated and more composable

--- home/scripts/dock-laptop.sh
+++ home/scripts/dock-laptop.sh
@@ -3,10 +3,10 @@
 # external_screen="DP-1-1.1"
 # internal_screen="eDP-1"
 
-notify-send "docking" -h string:fgcolor:#ffffff -h string:bgcolor:#00b373
+# notify-send "docking" -h string:fgcolor:#ffffff -h string:bgcolor:#00b373
 
 # ~/.screenlayout/vertical-2.sh || ~/.screenlayout/single.sh
-~/.screenlayout/triple.sh || ~/.screenlayout/single.sh
+# ~/.screenlayout/triple.sh || ~/.screenlayout/single.sh
 
 xmodmap ~/.xmodmap
 # killall yambar
@@ -18,9 +18,12 @@ xmodmap ~/.xmodmap
 #     xrandr --output "$internal_screen" --primary       # fallback to laptop
 
 dkcmd restart
-xbacklight-50
+# xbacklight-50
 CAPS_UNLOCK.SH # just in case I hit caps before xmodmap
-~/.xsession.d/xset.sh &>/dev/null &
-nitrogen --restore
+# ~/.xsession.d/xset.sh &>/dev/null &
+# nitrogen --restore
 # ~/.xsession.d/xcompmgr &>/dev/null &
 # yambar &
+bartender-bounce.sh
+killall trayer
+bartender.sh &
